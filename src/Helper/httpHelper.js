cb0f2424366994b23562985760461634f0674922
secabhi <abhiatstudy@gmail.com>
2017-04-14 08:24:52 +0530

    mapping of delaerproduct json with sendreqtobejson Done , post methoadded

--- src/Helper/httpHelper.js
+++ src/Helper/httpHelper.js
@@ -3,15 +3,35 @@
 import axios from 'axios';
 
 
-const HttpHelper = (url, method) => {
-    return (
-        //ajax stuff here
+const HttpHelper = (url, method, reqData) => {
+
+   //var reqData = {"dealer_code":123,"dealer_id":1234}
+
+    if (method.toLowerCase() == 'post') {
+        axios[method](url, reqData)
+            .then(function (response) {
+                console.log(response);
+                if (response.status == 200) {
+                    return response.data
+                } else {
+                    alert("Error while Fetching data");
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+    else {
         axios[method](url)
             .then(function (response) {
-                console.log(response.data); // ex.: { user: 'Your User'}
-                console.log(response.status); // ex.: 200
+                if (response.status == 200) {
+                    console.log(response.data);
+                    return response.data;
+                } else {
+                    alert("Error while Fetching data");
+                }
             })
-    );
+    }
 }
 
 export default HttpHelper;
--- src/components/Accordian/eData.js
+++ src/components/Accordian/eData.js
@@ -27,21 +27,41 @@ class Accordion extends React.Component {
         this.eMenuOptionselect = this.eMenuOptionselect.bind(this);
         this.editEMenu = this.editEMenu.bind(this);
         this.eMenuOnsave = this.eMenuOnsave.bind(this);
+        this.getMappedRequiredField = this.getMappedRequiredField.bind(this);
     }
 
-    componentDidMount(){
-        //this.state.dealerInfo = HttpHelper('url',methodtype)
-        this.state.dealerInfo = require('../../mockAPI/dealerProducts.json');
-        console.log(this.state.dealerInfo);
-        this.data.responseTomap = require('../../mockAPI/SendRequestToBE.json');
-        console.log(this.data.responseTomap);
-       //let mapppedval = _.omit(this.data.responseTomap,'Vehicle');
-       console.log(mapppedval);
-    }
-
-
-    getMappedRequiredField(){
+    componentDidMount() {
+        //console.log(HttpHelper("https://jsonplaceholder.typicode.com/posts/1",'get'))
+        //this.state.dealerInfo = HttpHelper('http://192.168.17.32:6100/api/dealer-products','get')/** Uncomment it and fetch the dealer product */
+        this.state.dealerProduct = require('../../mockAPI/dealerProducts.json');
+        console.log(this.state.dealerProduct);
+        // plz fetch SendRequestToBE
+        this.state.responseTomap = require('../../mockAPI/SendRequestToBE.json');
+        //this.state.responseTomap = HttpHelper(api,'get');
+        console.log(this.state.responseTomap);
+        //let mapppedval = _.omit(this.data.responseTomap,'Vehicle');
+        this.state.responseTomap.Products = this.getMappedRequiredField();
         
+        this.state.reqFieldResponseUI = require('../../mockAPI/reqFieldResponseUI.json');
+        //let requestData = HttpHelper('http://10.117.18.27:6220/Rating/RatingRESTAPI/json/requiredfields_json','post',this.state.responseTomap) /**uncomment it to fetch data from server for reqFieldResponseUI */
+    }
+
+
+    getMappedRequiredField() {
+        let responseTomap = this.state.responseTomap.Products;
+        let mappedData = [];
+        _.each(this.state.dealerProduct.results, function (item, i) {
+            if (item['is_rateable']) {
+                _.each(responseTomap, function (childitem,idx) {
+                    if ((item['category_code'] == childitem['ProductTypeCode'])
+                    &&(item['provider_code']==childitem['ProviderId'])
+                    &&(item['dealer_id']==childitem['ProviderDealerId'])){
+                        mappedData.push(childitem);
+                    }
+               });
+            }
+        });
+        return mappedData;
     }
 
     eMenuOptionselect(qid, optvalue) {
@@ -62,7 +82,7 @@ class Accordion extends React.Component {
                 })
 
         }
-        this.setState({"questions":this.state.questions});
+        this.setState({ "questions": this.state.questions });
         //console.log(this.data.eMenusecOne)
 
     }
@@ -83,29 +103,29 @@ class Accordion extends React.Component {
 
         return (
             <div>
-            <div className="row">
-            <div className="col-xs-12 nopadding-left">
-             <strong className="col-xs-2 nopadding-left">eMenu</strong><span className="col-xs-2"><span>$</span><span>total</span></span></div>
-            </div>
-            <div className="row rootborder">
-                {this.state.saveEMenu == false ? (<div className="col-xs-12 emenucol-head">
-                    <span className="emenuHead">Required Provider Question</span><strong style={{ float: 'right', cursor: 'pointer', textDecoration: 'underline', color: '#3f3fb5' }}
-                        onClick={this.editEMenu}>Edit</strong>
-                </div>) :
-                    (<section className="acc">
-                        <p className="emenuHead">Required Provider Question</p>
-                        <div className="col-xs-12">
+                <div className="row">
+                    <div className="col-xs-12 nopadding-left">
+                        <strong className="col-xs-2 nopadding-left">eMenu</strong><span className="col-xs-2"><span>$</span><span>total</span></span></div>
+                </div>
+                <div className="row rootborder">
+                    {this.state.saveEMenu == false ? (<div className="col-xs-12 emenucol-head">
+                        <span className="emenuHead">Required Provider Question</span><strong style={{ float: 'right', cursor: 'pointer', textDecoration: 'underline', color: '#3f3fb5' }}
+                            onClick={this.editEMenu}>Edit</strong>
+                    </div>) :
+                        (<section className="acc">
+                            <p className="emenuHead">Required Provider Question</p>
+                            <div className="col-xs-12">
 
-                            {
-                                this.state.questions.map((q, i) => {
-                                    return <Question key={i + 'q'} data={q} qId={i + 'q'} events={this.eMenuOptionselect} />
-                                })
-                            }
-                            <div className="btn btn-primary pull-right" onClick={this.eMenuOnsave}>Save</div>
-                        </div>
+                                {
+                                    this.state.questions.map((q, i) => {
+                                        return <Question key={i + 'q'} data={q} qId={i + 'q'} events={this.eMenuOptionselect} />
+                                    })
+                                }
+                                <div className="btn btn-primary pull-right" onClick={this.eMenuOnsave}>Save</div>
+                            </div>
 
-                    </section>)}
-            </div>
+                        </section>)}
+                </div>
             </div>
         )
     }
