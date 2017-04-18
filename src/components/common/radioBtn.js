0bcf628958723d2be67dc49d17cf5aaac666fa1b
secabhi <abhiatstudy@gmail.com>
2017-04-18 07:42:45 +0530

    radio btn responsive fix

--- src/components/common/radioBtn.js
+++ src/components/common/radioBtn.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Radio = (props) => {
     return (
-        <label className="choices radio-inline">
+        <label className="choices">
             <input type="radio" className="radio" 
             name={props.clientProductId+"-"+props.qId} checked={props.selected} 
             onChange={()=>props.events(props.clientProductId,props.clientProductId+"-"+props.qId,props.categoryName,props.data)} />
--- src/components/eMenu/reqProvider/question.js
+++ src/components/eMenu/reqProvider/question.js
@@ -6,15 +6,19 @@ import Radio from '../../common/radioBtn'
 const Question = (props) => {
     console.log("choices");
     return (
-        <div >
+        <div>
             <div>{props.data.Caption}</div>
-            <div className="control-group">
-                {
-                    _.map(props.data.FieldValues.FieldValue,function(c, i) {
-                        return <Radio key={props.clientproductId+"-"+i} data={c} categoryName={props.categoryName} clientProductId={props.clientproductId} selected={props.data.Value==c.Code?true:false} qId={props.qId} events={props.events} />
-                    })
-                }
-            </div>
+            <form>
+                <div className="radio">
+                    <div className="control-group">
+                        {
+                            _.map(props.data.FieldValues.FieldValue, function (c, i) {
+                                return <Radio key={props.clientproductId + "-" + i} data={c} categoryName={props.categoryName} clientProductId={props.clientproductId} selected={props.data.Value == c.Code ? true : false} qId={props.qId} events={props.events} />
+                            })
+                        }
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
