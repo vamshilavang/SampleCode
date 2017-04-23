0b84417a7d8b78070a6568f3d1ae572672c1c637
vamshilavang <vamshilavang2@gmail.com>
2017-04-23 12:50:57 +0530

    updated with server

---
+++ src/webpack.config.js
@@ -0,0 +1,28 @@
+require("babel-polyfill");
+
+module.exports = {
+  entry: ['babel-polyfill','./src/main.js'],
+  output: {
+    path: '/',
+    filename: 'index.js'
+  },
+  devServer: {
+    inline: true,
+    port:1300
+  },
+  module: {
+       loaders: [
+         {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader',
+          query: {
+              presets: ["react", "es2015"]
+         }
+     },{
+               test: /\.css$/,
+               loader: "style-loader!css-loader"
+           },
+       ]
+  }
+}
